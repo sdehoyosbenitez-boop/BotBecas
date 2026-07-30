0f65cc407a5e2837664fa70ac3e28dc2c3a73019
sdehoyosbenitez-boop <sdehoyosbenitez@gmail.com>
2026-07-30 10:31:47 -0500

    Update scoring_service.py

--- services/scoring_service.py
+++ services/scoring_service.py
@@ -1,1 +1,28 @@
+class ScoringService:
 
+    def score(self, scholarship):
+
+        score = 0
+
+        if scholarship.country in [
+            "Canada",
+            "United Kingdom",
+            "United States",
+            "Australia",
+            "New Zealand"
+        ]:
+            score += 15
+
+        if scholarship.coverage == "Full":
+            score += 30
+
+        if scholarship.level == "Bachelor":
+            score += 25
+
+        if scholarship.colombians:
+            score += 20
+
+        if scholarship.age >= 16:
+            score += 10
+
+        return score
