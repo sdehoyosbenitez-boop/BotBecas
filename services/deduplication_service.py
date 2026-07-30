6cda0afdc099890dc7e29a43f4f83f2630dfe836
sdehoyosbenitez-boop <sdehoyosbenitez@gmail.com>
2026-07-30 10:32:16 -0500

    Update deduplication_service.py

--- services/deduplication_service.py
+++ services/deduplication_service.py
@@ -1,1 +1,17 @@
+class DeduplicationService:
 
+    def remove_duplicates(self, scholarships):
+
+        urls = set()
+
+        unique = []
+
+        for scholarship in scholarships:
+
+            if scholarship.url not in urls:
+
+                urls.add(scholarship.url)
+
+                unique.append(scholarship)
+
+        return unique
