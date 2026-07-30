f6b39d540d690a4c06e6cd8dc867828b7b3b4461
sdehoyosbenitez-boop <sdehoyosbenitez@gmail.com>
2026-07-30 10:30:18 -0500

    Update search_service.py

--- services/search_service.py
+++ services/search_service.py
@@ -1,1 +1,17 @@
+class SearchService:
 
+    def __init__(self, collectors):
+        self.collectors = collectors
+
+    async def search(self):
+        scholarships = []
+
+        for collector in self.collectors:
+            try:
+                data = await collector.collect()
+                scholarships.extend(data)
+
+            except Exception as e:
+                print(e)
+
+        return scholarships
