f0c860a447f814d56223056971cd69afdd35d6e5
sdehoyosbenitez-boop <sdehoyosbenitez@gmail.com>
2026-07-30 10:37:27 -0500

    Update search_service.py

--- services/search_service.py
+++ services/search_service.py
@@ -1,17 +1,22 @@
+from typing import List
+
+from app.collectors.models import Scholarship
+
+
 class SearchService:
 
     def __init__(self, collectors):
+
         self.collectors = collectors
 
-    async def search(self):
+    async def search(self) -> List[Scholarship]:
+
         scholarships = []
 
         for collector in self.collectors:
-            try:
-                data = await collector.collect()
-                scholarships.extend(data)
 
-            except Exception as e:
-                print(e)
+            data = await collector.collect()
+
+            scholarships.extend(data)
 
         return scholarships
