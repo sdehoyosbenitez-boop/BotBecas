711dbf445d3b23a303a4c0557826b1e43ab44246
sdehoyosbenitez-boop <sdehoyosbenitez@gmail.com>
2026-07-30 10:33:27 -0500

    Update base.py

--- collectors/base.py
+++ collectors/base.py
@@ -1,5 +1,24 @@
-from abc import ABC, abstractmethod
+from abc import ABC
+
 class BaseCollector(ABC):
-    name="base"
-    @abstractmethod
-    async def collect(self): ...
+
+    name = ""
+
+    country = ""
+
+    source = ""
+
+    async def collect(self):
+
+        raise NotImplementedError
+
+    async def normalize(self, scholarship):
+
+        return scholarship
+
+    def validate(self, scholarship):
+
+        if scholarship.url == "":
+            return False
+
+        return True
